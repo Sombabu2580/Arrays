6bc84804203c0d6a8cefe48560de689000a16791
Sombabu2580 <119130792+Sombabu2580@users.noreply.github.com>
2022-12-09 21:22:50 +0530

    Add files via upload

---
+++ if else 1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	scanf("%d",&age);
+	
+	
+	
+	if(age>=18)
+	{
+	
+	printf("you can vote");
+}
+	else
+	{
+		printf("you can't vote");
+		return 0;
+	}
+}
